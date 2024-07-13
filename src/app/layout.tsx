4758c5eb846b48ee00d786dab7af8a0b188d0311
Miguel Chavez <toonchavez8@gmail.com>
2024-07-13 10:50:59 -0600

    chore(app): begin to add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import "@/app/styles/globals.css";
+import { DM_Sans } from "next/font/google";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" className="">
-			<body>{children}</body>
-			<PrismicPreview repositoryName={repositoryName} />
-		</html>
-	);
+  return (
+    <html lang="en" className="">
+      <body>{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
 }
